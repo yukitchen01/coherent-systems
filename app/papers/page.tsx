327ce43b242141399636cd04aa062f60bd4336e1
yukitchen01 <42143770+yukitchen01@users.noreply.github.com>
2024-10-08 12:02:14 +0900

    Update page.tsx
    Adjust size for smatphone

--- app/papers/page.tsx
+++ app/papers/page.tsx
@@ -1,13 +1,17 @@
 import '@/app/ui/global.css';
 
 export default function Paper() {
-    return (
-      <div>
-        {/* <h1>View PDF</h1> */}
-        <iframe
-          src="/papers/AMOS_Uchida_240830.pdf" // Update this with the path to your PDF
-          style={{ width: '100%', height: '850px' }}
-        />
-      </div>
-    );
-  }
+  return (
+    <div style={{ width: '100%', height: '100vh', overflow: 'auto' }}>
+      <iframe
+        src="/papers/AMOS_Uchida_240830.pdf"
+        style={{
+          width: '100%',
+          height: '100%',
+          border: 'none',
+        }}
+        title="PDF Viewer"
+      />
+    </div>
+  );
+}

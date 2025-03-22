037dc1d4b137646033c66b5a22799bcf566beaf1
yukitchen01 <42143770+yukitchen01@users.noreply.github.com>
2025-03-22 13:54:23 +0900

    Update page.tsx
    Changed font

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,13 +8,13 @@ export default function About() {
     <Head>
       <title>株式会社コヒーレントシステムズ - 会社概要</title>
           <meta name="description" content="株式会社コヒーレントシステムズの会社概要とメンバー紹介" />
-          <meta name="keywords" content="コヒーレント, レーザープラズマ, 再生可能エネルギー, 宇宙デブリ, Shigeaki Uchida, 内田成明, Kiyozumi Takeuchi, 竹内清澄" />
+          <meta name="keywords" content="コヒーレント, 会社概要, レーザー技術, 再生可能エネルギー, 宇宙デブリ" />
           <meta property="og:title" content="株式会社コヒーレントシステムズ - 会社概要" />
           <meta property="og:description" content="株式会社コヒーレントシステムズの会社概要、代表挨拶、研究内容などの詳細情報。" />
     </Head>
     <div className={styles.container}>
         <div className={styles.companyOverview}>
-            <h1>メンバー</h1>
+            <h1>Member</h1>
         </div>
       {/* Cards for CEO and CTO */}
       <div className={styles.cardsContainer}>
@@ -28,7 +28,8 @@ export default function About() {
             className={styles.cardImage}
           />
           <p>代表取締役 CEO</p>
-          <h2>竹内 清澄 (Kiyozumi Takeuchi)</h2>
+          <h1>竹内 清澄</h1>
+          <h2>(Kiyosumi Takeuchi)</h2>
           <p>慶応義塾大学 経済学部卒 プライム上場企業のマーケティング部長、
             執行役員(営業部長)等を歴任後2020年7月に知財マーケティングの事業化のために起業。
             2022年4月大阪大学と共同研究を開始。
@@ -47,13 +48,14 @@ export default function About() {
             className={styles.cardImage}
           />
           <p>取締役 CTO</p>
-          <h2>内田 成明 (Shigeaki Uchida)</h2>
+          <h1>内田 成明</h1>
+          <h2>(Nariaki Uchida)</h2>
           <p>大阪大学 工学部・工学研究科修了後、米国ロチェスター大学にて博士号取得。
             レーザー技術総合研究所主任研究員、東京工業大学特任教授、東京大学客員教授、光産業創成大学院大学
             客員教授、大阪大学招聘教授等を歴任。
             2022 年 4 月取締役兼レーザープラズマ応用研究所（大阪大学レーザー科学研究所との産学
             共同研究）所長に就任。現在、河南科技大(Henan university of Science and Technology)、
-            材料科学与工程学院 河南省招聘研究員、先進材料研究所客員教授。レーザー学会所属。
+            材料科学与工程学院 河南省招聘研究員、先进材料研究所客員教授。レーザー学会所属。
             </p>
             {/* <br/>
             <p>主な研究開発分野:</p>
@@ -71,7 +73,7 @@ export default function About() {
       <br/>
             
       <div className={styles.companyOverview}>
-        <h2>会社概要</h2>
+        <h2>Company Profile</h2>
         <section className={styles.section}>
             <p className={styles.title}>会社名</p>
             <p className={styles.content}>株式会社コヒーレントシステムズ</p>
@@ -130,27 +132,29 @@ export default function About() {
   
         <section className={styles.section}>
         <p className={styles.title}>特許</p>
-        <p className={styles.content}>特許第6644342号「振動評価装置及び振動評価システム」の専用実施権</p>
+        <p className={styles.content}>
+          特許第6644342号「振動評価装置及び振動評価システム」の専用実施権
+        </p>
         </section>
       </div>
 
         <div className={styles.companyOverview}>
-            <h2>代表挨拶</h2>
+            <h2>Message from CEO</h2>
             <p>企業の存在意義は、企業の生産するあらゆる製品やサービスなどを通して、人間社会
-                を豊かにすることにあります。日本企業は、戦後の日本型経営から21世紀にはグロー
+                を豊かにすることにあります。 日本企業は、戦後の日本型経営から21世紀にはグロー
                 バルスタンダード化し、米国に先導される形で株主価値の最大化と言う経済的価値の
-                追求に軸足を取った形で、収益性と成長性を高め経済発展を成し遂げてきました。
-                しかし、経済発展の裏側で起こっている地球環境など社会的問題の解決は先送りとなっ
-                ています。 これらの先送りされた課題に積極的に取り組む必要があると思います。 
-                今や企業にとって社会的責任を無視した経営は、支持を得ることは難しいでしょう。
-                株主資本主義による短期市場に翻弄されずに、長期的な視点で社会問題の解決に取り組
+                追求に軸足を取った形で、 収益性と成長性を高め経済発展を成し遂げてきました。し
+                かし、経済発展の裏側で起こっている地球環境など社会的問題の解決は先送りとなっ
+                ています。 これらの先送りされた課題に積極的に取り組む必要があると思います。 今
+                や企業にとって社会的責任を無視した経営は、支持を得ることは難しいでしょう。
+                株主資本主義による短期市場に翻弄されずに、 長期的な視点で社会問題の解決に取り組
                 まなければならないと考えます。私たちコヒーレントシステムズは、自社の強みであ
-                るレーザー科学や情報工学を駆使し、長期的な視野で戦略を構築し、社会的課題を解
+                るレーザー科学や情報工学を駆使し、 長期的な視野で戦略を構築し、社会的課題を解
                 決する技術を提供できる、そんな企業を目指しています。
             </p>
         <br/>
         <p className={styles.signature}>株式会社 コヒーレントシステムズ</p>
-        <p className={styles.signature}>代表取締役 竹内清澄</p>
+        <p className={styles.signature}>代表取締役CEO 竹内清澄</p>
         </div>
     </div>
     </>
